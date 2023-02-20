dd17e72c90577b2242ca5b8a9ffaeb69b5594f25
web-dev-rain <it.cloud.github@gmail.com>
2023-02-20 21:43:55 +0300

    add class_dialog.xml

--- app/src/main/java/com/example/attendanceapp/MainActivity.java
+++ app/src/main/java/com/example/attendanceapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.attendanceapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab_main);
+        fab.setOnClickListener(v-> showDialog());
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.class_dialog, null);
+
+        builder.setView(view);
     }
 }
