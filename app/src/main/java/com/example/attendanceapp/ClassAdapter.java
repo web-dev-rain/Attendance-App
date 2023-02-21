8fb917d95373db424ee212efd2074518f60c71dd
web-dev-rain <it.cloud.github@gmail.com>
2023-02-21 23:40:12 +0300

    add class_item.xml

--- app/src/main/java/com/example/attendanceapp/ClassAdapter.java
+++ app/src/main/java/com/example/attendanceapp/ClassAdapter.java
@@ -1,5 +1,32 @@
 package com.example.attendanceapp;
 
-public class ClassAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHolder> {
+    public static class ClassViewHolder extends RecyclerView.ViewHolder{
+
+        public ClassViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
