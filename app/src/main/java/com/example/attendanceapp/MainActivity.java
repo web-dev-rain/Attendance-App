e1ab5c28856960826d40594383df6cc04aa85a45
web-dev-rain <it.cloud.github@gmail.com>
2023-02-23 21:23:49 +0300

    add addClass method

--- app/src/main/java/com/example/attendanceapp/MainActivity.java
+++ app/src/main/java/com/example/attendanceapp/MainActivity.java
@@ -2,22 +2,28 @@ package com.example.attendanceapp;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-FloatingActionButton fab;
-RecyclerView recyclerView;
-ClassAdapter classAdapter;
-RecyclerView.LayoutManager layoutManager;
-ArrayList<ClassItem> classItems = new ArrayList<>();
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ClassItem> classItems = new ArrayList<>();
+
+    EditText class_edt;
+    EditText subject_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +31,35 @@ ArrayList<ClassItem> classItems = new ArrayList<>();
         setContentView(R.layout.activity_main);
 
         fab = findViewById(R.id.fab_main);
-        fab.setOnClickListener(v-> showDialog());
+        fab.setOnClickListener(v -> showDialog());
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+
+        classAdapter = new ClassAdapter(this, classItems);
+        recyclerView.setAdapter(classAdapter);
     }
 
-    private void showDialog(){
+    private void showDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View view = LayoutInflater.from(this).inflate(R.layout.class_dialog, null);
         builder.setView(view);
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
-        builder.create().show();
+        class_edt = view.findViewById(R.id.class_edt);
+        subject_edt = view.findViewById(R.id.subject_edt);
+
+        Button cancel = view.findViewById(R.id.cancel_btn);
+        Button add = view.findViewById(R.id.add_btn);
+
+        cancel.setOnClickListener(v -> dialog.dismiss());
+        add.setOnClickListener(v-> addClass());
+
+
+    }
+
+    private void addClass() {
     }
 }
